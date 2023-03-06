a5d3d101ad3d050686f78806b30a6e532d9f95c4
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-03-06 19:28:32 +0300

    clarifications added

--- seminar4_22.py
+++ seminar4_22.py
@@ -9,10 +9,30 @@ m - кол-во элементов второго множества.
 n = int(input("Cколько элементов в 1 списке?: "))
 m = int(input("Cколько элементов во 2 списке?: "))
 set_1 = [int(input("Вводите элемент 1го списка: ")) for i in range(n)]
-print(set_1)
+print(*set_1, sep=', ')
 set_1 = set(set_1)
 set_2 = [int(input("Вводите элемент 2го списка: ")) for i in range(m)]
-print(set_2)
+print(*set_2, sep=', ')
 set_2 = set(set_2)
 union = sorted(set_1.intersection(set_2))
 print(*union, sep=', ')
+
+# mol = [int(x) for x in input().split()]
+# n = mol[0]
+# m = mol[1]
+# set_1 = set()
+# set_2 = set()
+# list_1 = list()
+# a = [int(x) for x in input().split()]
+# k = set(a)
+# for i in k:
+#     set_1.add(i)
+# b = [int(x) for x in input().split()]
+# k1 = set(b)
+# for i in k1:
+#     set_2.add(i)
+# lok = set_1 & set_2
+# kool = list(lok)
+# kool.sort()
+# for i in kool:
+#     print(i, end=' ')
