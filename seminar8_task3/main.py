b90c87477230e5f4c2132998b7cc84404875f486
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-03-27 18:47:10 +0300

    add solution task_3 seminar_8

--- seminar8_task3/main.py
+++ seminar8_task3/main.py
@@ -18,3 +18,29 @@ ASCII(например, €);
 Реализовать считывание данных из созданного файла и проверить,
 совпадают ли они с исходными.
 """
+import yaml
+
+
+def yaml_save_data():
+    my_data = {
+        1: ['kristi', 'anton', 'karl'],
+        2: 4477,
+        3: {1: '\u04E0', 2: '\u14DF', 3: '\u0A07'}
+    }
+
+    with open('File.yaml', 'w', encoding='utf-8') as yaml_file:
+        yaml.dump(my_data, yaml_file, default_flow_style=False,
+                  allow_unicode=True)
+
+
+def read_yaml_file():
+    try:
+        with open('File.yaml', 'r', encoding='utf-8') as read_file:
+            content = yaml.Loader(read_file)
+            return content
+    except FileNotFoundError:
+        print('файла не существует')
+
+
+yaml_save_data()
+print(read_yaml_file())
