36541718d85060d063a80cd2c877420f477377e4
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-03-31 10:55:12 +0300

    add solution task_5 seminar_10

--- seminar10_task5.py
+++ seminar10_task5.py
@@ -5,3 +5,19 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+import subprocess
+import chardet
+
+args = ['ping', 'yandex.ru']
+yandex_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+print(yandex_ping.stdout)
+for line in yandex_ping.stdout:
+    res = chardet.detect(line)
+    print(line.decode(encoding=res['encoding']))
+
+args_youtube = ['ping', 'youtube.com']
+youtube_ping = subprocess.Popen(args_youtube, stdout=subprocess.PIPE)
+print(youtube_ping.stdout)
+for line in youtube_ping.stdout:
+    res = chardet.detect(line)
+    print(line.decode(encoding=res['encoding']))
