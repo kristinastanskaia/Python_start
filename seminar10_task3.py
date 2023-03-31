4c59bd7019e1b4c303098f3c4a7b507aaa08a7d1
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-03-31 10:38:21 +0300

    add solution task_3 seminar_10

--- seminar10_task3.py
+++ seminar10_task3.py
@@ -9,3 +9,15 @@
 --- обязательно!!! усложните задачу, "отловив" исключение,
 придумайте как это сделать
 """
+start_list = ['attribute', 'класс', 'функция', 'type']
+list_bytes = []
+list_exception = []
+for i in start_list:
+    if i.isascii():
+        list_bytes.append(bytes(i, "UTF-8"))
+    else:
+        list_exception.append(i)
+
+print(list_bytes)
+print(f"Невозможно записать в байтовом типе с помощью маркировки b'':"
+      f" {list_exception}")
