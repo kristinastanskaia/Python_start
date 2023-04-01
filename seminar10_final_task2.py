5867f0914f6e42c37ee54143cef3db9fd2068fef
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-04-01 16:25:05 +0300

    add solution task_2 seminar_10

--- seminar10_final_task2.py
+++ seminar10_final_task2.py
@@ -8,3 +8,15 @@
 --- b'class' - используйте маркировку b''
 --- используйте списки и циклы, не дублируйте функции
 """
+start_list = ['class', 'function', 'method']
+list_1 = []
+for i in start_list:
+    list_1.append(bytes(i, "UTF-8"))
+for i in list_1:
+    print(f'Слово в байтовом формате: {i}')
+    # for bytes in i:
+    #   print(bytes, end=' ')
+    print(f'Тип переменной: {type(i)}')
+    print(f'Содержимое переменной: {(str(i, "UTF-8"))}')
+    print(f'Длина переменной: {len(i)}')
+    print()
