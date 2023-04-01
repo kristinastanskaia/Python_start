6e061ee2d0c783d768a79ffd9745db7854ebd34d
KristinaStanskaia <kristina.stanskaia@gmail.com>
2023-04-01 16:29:17 +0300

    add solution task_4 seminar_10

--- seminar10_final_task4.py
+++ seminar10_final_task4.py
@@ -6,3 +6,13 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+start_list = ['разработка', 'администрирование', 'protocol', 'standard']
+list_encode = []
+list_decode = []
+for i in start_list:
+    list_encode.append(i.encode('utf-8'))
+print(f"В байтовом представлении:  {list_encode}")
+
+for i in list_encode:
+    list_decode.append(i.decode('utf-8'))
+print(f"В строковом представлении: {list_decode}")
